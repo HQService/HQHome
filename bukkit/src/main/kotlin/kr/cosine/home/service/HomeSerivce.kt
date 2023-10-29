eec15c107ed030c50999e88425686ff43ad1e827
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2023-10-29 16:32:13 +0900

    fix: 메시지 수정

--- bukkit/src/main/kotlin/kr/cosine/home/service/HomeSerivce.kt
+++ bukkit/src/main/kotlin/kr/cosine/home/service/HomeSerivce.kt
@@ -6,6 +6,7 @@ import kr.cosine.home.database.repository.HomeRepository
 import kr.cosine.home.location.impl.HomeLocationImpl
 import kr.cosine.home.netty.packet.TeleportToHomePacket
 import kr.cosine.home.registry.SettingRegistry
+import kr.cosine.home.registry.SettingRegistry.Companion.prefix
 import kr.hqservice.framework.bukkit.core.HQBukkitPlugin
 import kr.hqservice.framework.bukkit.core.coroutine.extension.BukkitMain
 import kr.hqservice.framework.global.core.component.Service
@@ -29,7 +30,7 @@ class HomeSerivce(
     suspend fun setHome(player: Player) {
         val playerUniqueId = player.uniqueId
         val port = nettyServer.getPlayer(playerUniqueId)?.getChannel()?.getPort() ?: run {
-            player.sendMessage("§c현재 채널의 포트를 불러오지 못했습니다.")
+            player.sendMessage("$prefix 현재 채널의 포트를 불러오지 못했습니다.")
             return
         }
         val playerLocation = player.location
@@ -47,17 +48,17 @@ class HomeSerivce(
         } else {
             homeRepository.create(playerUniqueId, homeDTO)
         }
-        player.sendMessage("현재 위치를 홈으로 설정하였습니다.")
+        player.sendMessage("$prefix 현재 위치를 홈으로 설정하였습니다.")
     }
 
     suspend fun home(player: Player) {
         val playerUniqueId = player.uniqueId
         val homeDTO = homeRepository.findByUniqueId(playerUniqueId) ?: run {
-            player.sendMessage("§c설정된 홈이 없습니다.")
+            player.sendMessage("$prefix 설정된 홈이 없습니다.")
             return
         }
         val channel = nettyServer.getChannel(homeDTO.port) ?: run {
-            player.sendMessage("§c채널을 불러오지 못했습니다.")
+            player.sendMessage("$prefix 채널을 불러오지 못했습니다.")
             return
         }
         countDownService.countDown(player) {
